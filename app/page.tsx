7f33c7b0ddb42ffbf0c31608cc3e608f98ab3697
Anne Merlyn Martinez <martinezannemerlyn@gmail.com>
2024-08-28 21:47:51 +0200

    adding sign-in button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 
 export default async function Home() {
     return (
-        <main className="">
+        <main className="text-neutral-50">
             <h1>Homepage</h1>
             <Button>Click Me</Button>
         </main>
--- components/navigation/nav.tsx
+++ components/navigation/nav.tsx
@@ -1,17 +1,31 @@
 import { auth } from '@/server/auth';
 import { UserButton } from './user-button';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { LogIn } from 'lucide-react';
 
 export default async function Nav() {
     const session = await auth();
 
     return (
-        <header className="bg-slate-500 py-4">
+        <header>
             <nav>
-                <ul className="flex justify-between">
+                <ul className="flex justify-between text-neutral-50">
                     <li>Logo</li>
-                    <li>
-                        <UserButton session={session} />
-                    </li>
+                    {!session ? (
+                        <li>
+                            <Button asChild>
+                                <Link className="flex" href="/auth/login">
+                                    <LogIn size={16} />
+                                    <span>LogIn</span>
+                                </Link>
+                            </Button>
+                        </li>
+                    ) : (
+                        <li>
+                            <UserButton session={session} />
+                        </li>
+                    )}
                 </ul>
             </nav>
         </header>
